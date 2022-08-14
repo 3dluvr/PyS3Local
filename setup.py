792fc0516daeb00533fb5cadf7e3723aa5027b4c
3dluvr <3dluvr@users.noreply.github.com>
2022-08-14 15:03:57 -0400

    Update to Python3

---
+++ setup.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+PROJECT = 'PyS3Local'
+
+# Change docs/sphinx/conf.py too!
+VERSION = '0.1'
+
+from setuptools import setup, find_packages
+
+try:
+    long_description = open('README.rst', 'rt').read()
+except IOError:
+    long_description = ''
+
+setup(
+    name=PROJECT,
+    version=VERSION,
+
+    description='Fork of mock-s3, a Python clone of fake-s3, A lightweight server clone of Amazon S3.',
+    long_description=long_description,
+
+    author='3dluvr',
+    author_email='3dluvr@gmail.com',
+
+    url='https://github.com/3dluvr/PyS3Local',
+    download_url='https://github.com/3dluvr/PyS3Local/tarball/master',
+
+    classifiers=['Development Status :: 3 - Alpha',
+                 'License :: OSI Approved :: MIT',
+                 'Programming Language :: Python',
+                 'Programming Language :: Python :: 3',
+                 'Intended Audience :: Developers',
+                 'Environment :: Console',
+                 ],
+
+    platforms=['Any'],
+
+    scripts=[],
+
+    provides=[],
+    install_requires=[],
+
+    namespace_packages=[],
+    packages=find_packages(),
+    include_package_data=True,
+
+    entry_points={
+        'console_scripts': [
+            'pys3local = pys3local.main:main'
+            ],
+        },
+
+    zip_safe=False,
+)
